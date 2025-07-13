5d3f0b6b3c92a996e02cea5c6ff311c06a89daff
ghislaingenay <ghislain.gny@gmail.com>
2025-07-13 13:51:59 +0700

    feat: refactor user views to implement JSON:API format for token authentication

--- server/formsystem/urls.py
+++ server/formsystem/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
 
-
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
