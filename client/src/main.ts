470e564f05eabd7f3d24e591af31c69f64621530
ghislaingenay <ghislain.gny@gmail.com>
2025-07-16 21:28:00 +0700

    feat: integrate Vue3Toastify for toast notifications and update TypeScript configuration

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+  } as ToastContainerOptions)
+  .mount("#app");
