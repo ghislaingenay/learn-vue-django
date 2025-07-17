0dc7bcd415a2dc1be59cda094fe69b2e002b8500
ghislaingenay <ghislain.gny@gmail.com>
2025-07-17 22:27:31 +0700

    feat: integrate Vuetify for enhanced UI components in main application

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,19 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createVuetify } from "vuetify";
 import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
 import router from "./router";
+
+const vuetify = createVuetify({ components, directives });
 
 const app = createApp(App);
 app.use(Vue3Toastify, {
   autoClose: 3000,
 } as ToastContainerOptions);
 app.use(router);
+app.use(vuetify);
 app.mount("#app");
