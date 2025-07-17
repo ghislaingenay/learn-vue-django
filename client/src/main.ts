e093f36df5ca99b3b15d6eae1917d5ac91064977
ghislaingenay <ghislain.gny@gmail.com>
2025-07-17 20:50:50 +0700

    feat: add vue3-toastify for notifications and update Vite config with path aliases

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+import router from "./router";
 
-createApp(App)
-  .use(Vue3Toastify, {
-    autoClose: 3000,
-  } as ToastContainerOptions)
-  .mount("#app");
+const app = createApp(App);
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+} as ToastContainerOptions);
+app.use(router);
+app.mount("#app");
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,5 +5,27 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tsconfigPaths(), tailwindcss()],
+  plugins: [
+    vue(),
+    tsconfigPaths({
+      root: "./",
+      projects: ["./tsconfig.app.json"],
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@services": "/src/services",
+      "@constants": "/src/constants",
+      "@definitions": "/src/definitions",
+      "@types": "/src/types",
+      "@router": "/src/router",
+      "@validators": "/src/validators",
+      "@utils": "/src/utils",
+      "@modules": "/src/modules",
+    },
+  },
 });
