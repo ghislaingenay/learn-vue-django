da9925df287b09b25cd8c40136094e3a7da5310d
ghislaingenay <ghislain.gny@gmail.com>
2025-07-18 22:36:03 +0700

    feat: implement JWT validation and token refresh logic in AuthService

---
+++ client/src/constants/regex.ts
@@ -0,0 +1,2 @@
+export const JWT_REGEX = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
+export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -3,6 +3,8 @@ import type { UserRegistration } from "@types";
 import axios from "axios";
 import BaseService from "./base";
 import ERROR_CODES from "@constants/error_code";
+import { JWT_REGEX } from "@constants/regex";
+import { jwtDecode } from "jwt-decode";
 
 export default class AuthService extends BaseService {
   constructor() {
@@ -11,7 +13,8 @@ export default class AuthService extends BaseService {
     });
   }
 
-  static async isAuthenticated(): Promise<boolean> {
+  async isAuthenticated(): Promise<boolean> {
+    await this.requestNewAccessToken(); // Ensure token is refreshed if needed
     const accessToken = localStorage.getItem("access_token");
     if (!accessToken) return false;
 
@@ -28,9 +31,53 @@ export default class AuthService extends BaseService {
     }
   }
 
+  private getExpirationDate(): Date | null {
+    const isValidToken = this.isValidToken();
+    if (!isValidToken) return null;
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) return null;
+    const decodedToken = jwtDecode<{ exp: number }>(accessToken);
+    return decodedToken.exp ? new Date(decodedToken.exp * 1000) : null;
+  }
+
+  async requestNewAccessToken(
+    timeMinBeforeExpiration: number = 5
+  ): Promise<void> {
+    const expirationDate = this.getExpirationDate();
+    if (!expirationDate) {
+      localStorage.removeItem("access_token");
+      return; // Token is invalid or not found
+    }
+
+    const currentTime = Date.now();
+    const timeToExpiration = expirationDate.getTime() - currentTime;
+
+    const needsRefresh =
+      timeToExpiration < timeMinBeforeExpiration * 60 * 1000 ||
+      timeToExpiration <= 0;
+    if (needsRefresh) {
+      await this.refreshToken();
+      return;
+    }
+
+    return; // Token is still valid
+  }
+
+  private isValidToken(): boolean {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) return false;
+    if (!JWT_REGEX.test(accessToken)) return false;
+    return jwtDecode<{ exp: number }>(accessToken).exp > Date.now() / 1000;
+  }
+
   async refreshToken(): Promise<void> {
     const accessToken = localStorage.getItem("access_token");
     if (!accessToken) throw new Error(ERROR_CODES.E_ACCESS_TOKEN_NOT_FOUND);
+    const response = await this._axios.post("/jwt/refresh", {
+      access: accessToken,
+    });
+    localStorage.setItem("access_token", response.data.access_token);
+    return response.data;
   }
 
   async register(body: UserRegistration): Promise<void> {
