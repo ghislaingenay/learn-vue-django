8dfe4ae3710c283c2672b55c94ffb12469605c13
ghislaingenay <ghislain.gny@gmail.com>
2025-07-19 21:00:02 +0700

    feat: add Pinia for state management and update main application setup

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,23 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+
 import "vuetify/styles/main.css";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { createVuetify } from "vuetify";
 import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
 import router from "./router";
+import { createPinia } from "pinia";
 
 const vuetify = createVuetify({ components, directives });
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(Vue3Toastify, {
   autoClose: 3000,
 } as ToastContainerOptions);
 app.use(router);
 app.use(vuetify);
+app.use(pinia);
 app.mount("#app");
