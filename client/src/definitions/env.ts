a4f25a4ad808bded4d500daf79d1e00fd021a90c
ghislaingenay <ghislain.gny@gmail.com>
2025-07-19 12:17:05 +0700

    fix: remove API version from base URL in Env class

--- client/src/definitions/env.ts
+++ client/src/definitions/env.ts
@@ -2,7 +2,7 @@ export default class Env {
   static BACKEND_ENDPOINT =
     import.meta.env.VITE_BACK_ENDPOINT || "http://localhost:8000";
   static API_VERSION = "v1";
-  static API_BASE_URL = `${Env.BACKEND_ENDPOINT}${Env.API_VERSION}/`;
+  static API_BASE_URL = `${Env.BACKEND_ENDPOINT}`;
   static getApiUrl(endpoint: string = ""): string {
     return `${Env.API_BASE_URL}${endpoint}`;
   }
